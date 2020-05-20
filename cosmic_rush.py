249473f48bfca894cf7d20b97934c2b24688b670
Buster Blue <buster_blue@protonmail.com>
2020-05-20 19:57:54 -0400

    Make a working window

--- cosmic_rush.py
+++ cosmic_rush.py
@@ -1,2 +1,25 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import arcade
+import time
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        arcade.start_render()
+
+
+arcade.Window()
+# Keep the window open for 5 seconds, as a placeholder
+time.sleep(5)
